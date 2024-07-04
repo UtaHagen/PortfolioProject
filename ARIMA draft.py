a936c16ada4f4e767ca5b55bbb391abae4781ed5
Brian Sung <brian.sung.work@gmail.com>
2024-07-03 23:16:06 -0700

    arima draft

---
+++ ARIMA draft.py
@@ -0,0 +1,37 @@
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+import numpy as np
+
+tickerSymbols = ['AAPL', '^GSPC']
+
+for tickerSymbol in tickerSymbols:
+    tickerData = yf.Ticker(tickerSymbol)
+
+    tickerDf = tickerData.history(period='10y')
+
+    tickerDf['Pct_Change'] = tickerDf['Close'].pct_change() * 100
+
+    monthly_average = tickerDf['Pct_Change'].resample('ME').mean()
+
+#cleaning monthly_average to address DateTime
+monthly_average = monthly_average.reset_index(drop = True)
+
+#examining the data
+monthly_average.plot(label = tickerSymbol)
+
+msk = (monthly_average.index < len(monthly_average)-30)
+df_train = monthly_average[msk].copy()
+df_test = monthly_average[~msk].copy()
+
+print(df_train)
+
+#Checking for stationarity of time series
+
+#Method 1 - ACF and PACF plot
+#acf_original = plot_acf(df_train)
+
+#pacf_original = plot_pacf(df_train)
+
+#plt.show()
