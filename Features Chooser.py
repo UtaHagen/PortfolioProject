a2f37f51ca0766e203d98c8b4642d12af6720b7f
Brian Sung <brian.sung.work@gmail.com>
2024-07-10 15:00:10 -0700

    scaffold an example of aapl stock running through the combinatorics code

--- Features Chooser.py
+++ Features Chooser.py
@@ -1,14 +1,24 @@
 import pandas as pd
+import yfinance as yf
 import statsmodels.api as sm
 from itertools import chain, combinations
 
+#Data port + manipulation
+aapl = yf.download('AAPL', start='2010-01-01', end='2024-01-01', progress=False)
+monthly_aapl = aapl.resample('ME').mean()
 
+high14 = monthly_aapl['High'].rolling(14).max()
+low14 = monthly_aapl['Low'].rolling(14).min()
+monthly_aapl['Williams %R'] = -100 * ((high14 - monthly_aapl['Close']) / (high14 - low14))
+monthly_aapl['Return'] = monthly_aapl['Close'].pct_change()
 
-y = df['y']
-X = df.drop('y', axis=1)
+monthly_aapl.dropna(inplace=True)
+
+y = monthly_aapl['Return']
+X = monthly_aapl.drop('Return', axis=1)
 
 def powerset(iterable):
-        s = list(iterable)
+    s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(1, len(s)+1))
 
 
