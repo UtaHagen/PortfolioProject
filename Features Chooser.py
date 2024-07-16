a7b250dac62175a1ab38e880a0a9933a841ff568
Brian Sung <brian.sung.work@gmail.com>
2024-07-15 23:01:06 -0700

    hmm messed around with the data to get everything to be the same length

--- Features Chooser.py
+++ Features Chooser.py
@@ -3,19 +3,35 @@ import yfinance as yf
 import statsmodels.api as sm
 from itertools import chain, combinations
 
-#Data port + manipulation
+#Data port + manipulation (aapl)
 aapl = yf.download('AAPL', start='2010-01-01', end='2024-01-01', progress=False)
 monthly_aapl = aapl.resample('ME').mean()
 
+#Federal Funds Port + Manipulation
+ffunds_path = [r'C:\Users\bsung\OneDrive\Documents\GitHub\PortfolioProject\Data\FEDFUNDS.csv']
+ffunds_path_str = ffunds_path[0]
+ffunds = pd.read_csv(ffunds_path_str)
+
+ffunds = ffunds.drop(ffunds.index[0])
+ffunds['DATE'] = pd.to_datetime(ffunds['DATE'])
+ffunds.set_index('DATE', inplace=True)
+ffunds.index = ffunds.index.to_period('M').to_timestamp('M')
+
+#William %R - Data manipulation
 high14 = monthly_aapl['High'].rolling(14).max()
 low14 = monthly_aapl['Low'].rolling(14).min()
 monthly_aapl['Williams %R'] = -100 * ((high14 - monthly_aapl['Close']) / (high14 - low14))
 monthly_aapl['Return'] = monthly_aapl['Close'].pct_change()
 
-monthly_aapl.dropna(inplace=True)
+#Combining Data
+data = pd.DataFrame(index=aapl.index)
+data['Williams %R'] = monthly_aapl['Williams %R']
+data['ffunds'] = ffunds['FEDFUNDS']
+
+data.dropna(inplace=True)
 
 y = monthly_aapl['Return']
-X = monthly_aapl.drop('Return', axis=1)
+X = data[['Williams %R','ffunds']]
 
 def powerset(iterable):
     s = list(iterable)
---
+++ testing area.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import yfinance as yf
+import statsmodels.api as sm
+from itertools import chain, combinations
+
+#Data port + manipulation (aapl)
+aaplWR = yf.download('AAPL', start='2008-12-31', end='2024-01-01', progress=False)
+aapl = yf.download('AAPL', start='2010-01-01', end='2024-01-01', progress=False)
+monthly_aaplWR = aaplWR.resample('ME').mean()
+monthly_aapl = aapl.resample('ME').mean()
+
+#Federal Funds Port + Manipulation
+ffunds_path = [r'C:\Users\bsung\OneDrive\Documents\GitHub\PortfolioProject\Data\FEDFUNDS.csv']
+ffunds_path_str = ffunds_path[0]
+ffunds = pd.read_csv(ffunds_path_str)
+
+ffunds = ffunds.drop(ffunds.index[0])
+ffunds['DATE'] = pd.to_datetime(ffunds['DATE'])
+ffunds.set_index('DATE', inplace=True)
+ffunds.index = ffunds.index.to_period('M').to_timestamp('M')
+
+#William %R - Data manipulation
+monthly_aapl_WR = pd.DataFrame()
+monthly_aapl_return_WR = pd.DataFrame()
+high14 = monthly_aaplWR['High'].rolling(14).max()
+low14 = monthly_aaplWR['Low'].rolling(14).min()
+monthly_aapl_return_WR['Williams %R'] = -100 * ((high14 - monthly_aapl['Close']) / (high14 - low14))
+
+monthly_aapl['Return'] = monthly_aapl['Close'].pct_change()
+
+#Combining Data
+data = pd.DataFrame(index=aaplWR.index)
+data['Williams %R'] = monthly_aapl['Williams %R']
+data['ffunds'] = ffunds['FEDFUNDS']
+
+
+
+print(monthly_aapl_return_WR)
