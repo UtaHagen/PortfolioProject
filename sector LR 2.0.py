e16fced51b39963f3e2a5a8212c07d4829ea16ad
Brian Sung <brian.sung.work@gmail.com>
2024-08-25 11:55:46 -0700

    changes horray!

--- sector LR 2.0.py
+++ sector LR 2.0.py
@@ -12,14 +12,13 @@ monthly_sp500['Return'] = (monthly_sp500['Close'].shift(-21) - monthly_sp500['Cl
 sectors_path = r'C:\Users\bsung\OneDrive\Documents\GitHub\PortfolioProject\Data\sectorsxd - Sheet1.csv'
 sectors_df = pd.read_csv(sectors_path)
 
-# Create an empty DataFrame to store the results
 results_df = pd.DataFrame(columns=['Sector', 'Intercept', 'Coefficient', 'R^2', 'P-Value'])
 
 for sector_symbol in sectors_df['Sector']:
     print(f'Processing sector: {sector_symbol}')
     
     sector_df = yf.download(sector_symbol, start='2010-01-01')
-    monthly_sector = sector_df.resample('M').mean()
+    monthly_sector = sector_df.resample('ME').mean()
     monthly_sector['Return'] = (monthly_sector['Close'].shift(-21) - monthly_sector['Close']) / monthly_sector['Close']
 
     monthly_sp500.dropna(inplace=True)
@@ -42,22 +41,11 @@ for sector_symbol in sectors_df['Sector']:
 
     r2 = model.rsquared
 
-    # Store the results in the DataFrame
     new_row = pd.DataFrame({
         'Sector': [sector_symbol],
-        'Intercept': [model.params[0]],
         'Coefficient': [model.params[1]],
-        'R^2': [r2],
-        'P-Value': [model.pvalues[1]]
     })
 
     results_df = pd.concat([results_df, new_row], ignore_index=True)
 
-    print(f'Sector: {sector_symbol}')
-    print(f'Intercept: {model.params[0]}')
-    print(f'Coefficients: {model.params[1:]}')
-    print(f'R^2 Score: {r2}')
-    print(f'P-Values: \n{model.pvalues}')
-
-# Save the results DataFrame to a CSV file
 results_df.to_csv(r'C:\Users\bsung\OneDrive\Documents\GitHub\PortfolioProject\Data\sector LR results.csv', index=False)
