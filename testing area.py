f08e68060df4522e83bd59a0dd43ae1405b698f6
Brian Sung <brian.sung.work@gmail.com>
2024-11-03 11:05:59 -0800

    long puts options

--- testing area.py
+++ testing area.py
@@ -1,30 +1,8 @@
-import yfinance as yf
-import pandas as pd
-from datetime import datetime
+import requests
 
-# Load the portfolio data
-monthly_returns_path = r'Data\portfolio_pick.csv'
-monthly_returns = pd.read_csv(monthly_returns_path)
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+url = 'https://www.alphavantage.co/query?function=HISTORICAL_OPTIONS&symbol=IBM&apikey=6WLY2DVR35CPCC7A'
+r = requests.get(url)
+data = r.json()
 
-def calculate_most_recent_monthly_return(ticker):
-    stock = yf.Ticker(ticker)
-    hist = stock.history(period="2mo")
-    hist['Monthly Return'] = hist['Close'].pct_change(periods=21)
-    hist = hist[['Monthly Return']].dropna()
-
-    today = datetime.today()
-    most_recent_return = hist.iloc[-1] if today.day == 1 else hist.iloc[-2]
-
-    return most_recent_return['Monthly Return']
-
-# Calculate monthly returns for each stock
-stock_dataframes = {ticker: calculate_most_recent_monthly_return(ticker) for ticker in monthly_returns['Stock']}
-
-# Create a DataFrame with the results
-result_df = pd.DataFrame(list(stock_dataframes.items()), columns=['Stock', 'Monthly Return'])
-
-# Get the top 10 tickers with the highest returns
-top_10_df = result_df.nlargest(10, 'Monthly Return')
-
-# Output the result to a CSV file
-top_10_df.to_csv('top10portfoliopick.csv', index=False)
+print(data)
